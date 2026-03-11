e9c229c9a5ebfc2a9677725deea778e65cab5cb6
SHARVAN223 <sharvan70458@gmail.com>
2026-03-11 23:20:37 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,68 @@
-import { useSelector, useDispatch } from "react-redux"
-import { addTask, deleteTask, completeTask, incompleteTask } from "./TodoSlice";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import { addTask,recDel,taskComp,taskIncomp,editDataSave} from "./TodoSlice";
+const App=()=>{
+  const [txtval,settxtval]=useState("");
+  const [btnStatus,setBtnStatus]=useState(true);
+  const [taskId,settaskId]=useState("");
+  const  task= useSelector(state=>state.todo.task);
+  const dispatch=useDispatch();
+  console.log(task);
 
-const App = () => {
-
-  const [txtval, setTxtVal] = useState("");
-
-  const task = useSelector(state => state.todo.task);
-
-  const dispatch = useDispatch()
-
-  let sno = 0;
-
-  const ans = task.map((key, index) => {
-
-    sno++;
-
-    return (
-      <tr
-        key={index}
-        style={{
-          color: key.status ? "red" : "black",
-          textDecoration: key.status ? "line-through" : "none"
-        }}
-      >
-        <td>{sno}</td>
-        <td>{key.work}</td>
-
-        <td>
-          <button onClick={() => dispatch(completeTask(index))}>
-            Complete
-          </button>
-        </td>
-
-        <td>
-          <button onClick={() => dispatch(incompleteTask(index))}>
-            Incomplete
-          </button>
-        </td>
-
-        <td>
-          <button onClick={() => dispatch(deleteTask(index))}>
-            Delete
-          </button>
-        </td>
-
+  let sno=0;
+    const ans=task.map((key)=>{
+      sno++;
+      return(<>
+      <tr>
+      <td>{sno}</td>
+      <td>{key.complete ?(
+        <>
+        <span style={{color:'red',textDecoration:'line-through'}}>{key.work}</span>
+        </> ):(
+          <>
+          {key.work}
+          </>
+        )
+      }</td>
+      <td>
+        <button onClick={()=>dispatch(recDel({id:key.id}))}>Delete</button>
+      </td>
+      <td>
+        <button onClick={()=>dispatch(taskComp({id:key.id}))}>Complete</button>
+      </td>
+      <td>
+        <button onClick={()=>dispatch(taskIncomp({id:key.id}))}>Incomplete</button>
+      </td>
+      <td>
+        <button onClick={()=>{myEdit(key.work,key.id)}}>Edit</button>
+      </td>
+      
       </tr>
-    )
-  })
-
-  return (
+      
+      </>)
+    })
+  return(
     <>
-      <h1>To Do App</h1>
 
-      Enter Task :
-      <input
-        type="text"
-        value={txtval}
-        onChange={(e) => setTxtVal(e.target.value)}
-      />
+      <h1> To do App</h1>
+      Enter task:<input type="text" value={txtval} onChange={(e)=>{settxtval(e.target.value)}}/>
+      {btnStatus ? (<>
+        <button onClick={()=>{dispatch(addTask({id:Date.now(),work:txtval}))}}>Add</button>
 
-      <button onClick={() => {
-        dispatch(addTask({ work: txtval }))
-        setTxtVal("")
-      }}>
-        Add Task
-      </button>
-
-      <hr />
-
+      </>):
+      (<>
+             <button onClick={myEditSave}>Save</button>
+      </>)}
+      <hr/>
       <table border="1">
-
         <tr>
-          <th>SNO</th>
-          <th>Task</th>
-          <th>Complete</th>
-          <th>Incomplete</th>
-          <th>Delete</th>
+          <th>S.No.</th>
+          <th>Your tasks</th>
+          <th colSpan={3}>Actions</th>
+          
         </tr>
-
         {ans}
-
       </table>
-
     </>
   )
 }
--- src/TodoSlice.jsx
+++ src/TodoSlice.jsx
@@ -1,37 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-const TodoSlice = createSlice({
-  name: "Todo",
-
-  initialState: {
-    task: []
-  },
-
-  reducers: {
-
-    addTask: (state, action) => {
-      state.task.push({
-        work: action.payload.work,
-        status: false
-      })
+const todoSlice=createSlice({
+    name:"todo",
+    initialState:{
+        task:[]
     },
-
-    deleteTask: (state, action) => {
-      state.task.splice(action.payload, 1)
-    },
-
-    completeTask: (state, action) => {
-      state.task[action.payload].status = true
-    },
-
-    incompleteTask: (state, action) => {
-      state.task[action.payload].status = false
+    reducers:{
+        addTask:(state,actions)=>{
+            console.log(actions.payload);
+            state.task.push(actions.payload)
+        },
+        recDel:(state,actions)=>{
+            state.task=state.task.filter(key=>key.id!=actions.payload.id);
+        },
+        taskComp:(state,actions)=>{
+            for(var i=0;i<state.task.length;i++){
+                if(state.task[i].id==actions.payload.id){
+                    state.task[i].complete=true
+                }
+            }
+        },
+        taskIncomp:(state,actions)=>{
+            for(var i=0;i<state.task.length;i++){
+                if(state.task[i].id==actions.payload.id){
+                    state.task[i].complete=false
+                }
+            }
+        },
+        editDataSave:(state,actions)=>{
+            console.log(actions.payload);
+            for(var i=0;i<state.task.length;i++){
+                if(state.task[i].id==actions.payload.id){
+                    state.task[i].work=actions.payload.work;
+                }
+            }
+        }
     }
-
-  }
-
 })
 
-export const { addTask, deleteTask, completeTask, incompleteTask } = TodoSlice.actions;
-
-export default TodoSlice.reducer;
+export const{addTask,recDel,taskComp,taskIncomp,editDataSave}=todoSlice.actions;
+export default todoSlice.reducer;
